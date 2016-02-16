6afb12cdab7270556eb7bed01807595c4a355db7
William Zhang <wzhang4@stanford.edu>
2016-02-15 18:52:32 -0800

    made code into function, reshaped to 48*48, separated train from val and test

--- process_data.py
+++ process_data.py
@@ -5,21 +5,62 @@ csv_file = open('../fer2013/fer2013.csv')
 
 reader = csv.reader(csv_file)
 
-# Discard header
-row = next(reader)
+def read_faces_csv(reader_file, center=True):
+	"""
+	Function that takes as input file a csv.reader() instance and assumes the following formatting:
+	emotion, pixels (2034 of them), usage (train, test, val)
+	Returns the following numpy arrays:
+	- X_train, y_train (respectively (N, 48, 48), (N,) representing raw grayscale pixels and emotion labels)
+	- X_test, y_test
+	- X_val, y_val
+	"""
 
-y_list = []
-X_list = []
+	# Discard header
+	row = next(reader_file)
 
-for row in reader:
-	y_str, X_row_str = (row[0], row[1])
-	y = int(y_str)
-	y_list.append([y])
+	X_train_list, y_train_list = [], []
+	X_test_list, y_test_list = [], []
+	X_val_list, y_val_list = [], []
 
-	X_row_strs = X_row_str.split(' ')
-	X_row = [float(x) for x in X_row_strs]
-	X_list.append(X_row)
+	N_train, N_test, N_val = 0, 0, 0
 
-X = np.asarray(X_list).astype('float64')
+	for row in reader_file:
+		y_str, X_row_str, data_type = row
+		y = int(y_str)
 
-y = np.asarray(y_list)
+		X_row_strs = X_row_str.split(' ')
+		X_row = [float(x) for x in X_row_strs]
+		
+		if data_type == 'PublicTest':
+			y_test_list.append(y)
+			X_test_list.append(X_row)
+			N_test += 1
+		elif data_type == 'PrivateTest':
+			y_val_list.append(y)
+			X_val_list.append(X_row)
+			N_val += 1
+		else:
+			y_train_list.append(y)
+			X_train_list.append(X_row)
+			N_train += 1
+
+	X_train = np.asarray(X_train_list).astype('float64').reshape((N_train, 48, 48))
+	y_train = np.asarray(y_train_list)
+
+	X_test = np.asarray(X_test_list).astype('float64').reshape((N_test, 48, 48))
+	y_test = np.asarray(y_test_list)
+
+	X_val = np.asarray(X_val_list).astype('float64').reshape((N_val, 48, 48))
+	y_val = np.asarray(y_val_list)
+
+	# decide to mean-center or not
+	if center:
+		train_mean = X_train.mean(axis = 0)
+		X_train -= train_mean
+		X_test -= train_mean
+		X_val -= train_mean
+
+	#########
+	return X_train, y_train, X_test, y_test, X_val, y_val
+
+X_train, y_train, X_test, y_test, X_val, y_val = read_faces_csv(reader)
